befef28a63c501ca3b2f5f0036b2738f522e7f4a
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-04-17 12:45:16 +0530

    Update Array_&_ArrayList.java

--- 📂 java-revision/Array_&_ArrayList.java
+++ 📂 java-revision/Array_&_ArrayList.java
@@ -1,3 +1,4 @@
 datatype{] variable_name = new datatype
 // syntax :
-  
+    datatype[] variable_name = new datatype[size];
+	int[] var1 = new int[5]; // this line means that the reference variable "var1" is pointing an array object that ype is integer.
