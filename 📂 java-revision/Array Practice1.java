862639d3ea921d6e535700fd00c73f6cad85b8be
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-04-17 14:59:17 +0530

    Update Array Practice1.java

--- 📂 java-revision/Array Practice1.java
+++ 📂 java-revision/Array Practice1.java
@@ -5,7 +5,7 @@ public class Input {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
       int[] arr1 = new int[5];
-      for(int i=0;i<=arr1.length;i++){
+      for(int i=0;i<arr1.length;i++){
         System.out.println("The numbers are:" + arr1);
       }
   }
