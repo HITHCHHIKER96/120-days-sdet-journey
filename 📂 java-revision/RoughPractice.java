ebef828ab0eadce3d3a4b4ce49672ab1960fb987
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-04-18 20:09:43 +0530

    Update RoughPractice.java

--- 📂 java-revision/RoughPractice.java
+++ 📂 java-revision/RoughPractice.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class Myclass {
     public static void main(String[] args){
-        System.out.println("Hello");
+        // System.out.println("Hello");
+        int[] arr = {23, 6, 87, 10};
+        System.out.println(Arrays.toString(arr));
+        changeMe(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void changeMe{
+        arr[0] = 3;
     }
 }
