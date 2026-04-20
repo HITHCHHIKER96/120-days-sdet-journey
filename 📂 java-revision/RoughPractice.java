119153dd4e8ec9113b9c8f627b578a30228cfd43
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-04-20 11:34:35 +0530

    Update RoughPractice.java

--- 📂 java-revision/RoughPractice.java
+++ 📂 java-revision/RoughPractice.java
@@ -16,7 +16,7 @@ public class Myclass {
     int[][] arr = new int[][]; // so here is [][] defined by 2d arrays and most important is when the initialisation part happened first [] is for row and second [] is for coloumn and the first one size you have to define as it is mandatory, but the second one is not mandatory.
     like int[][] arr1 = new in[2][]; // you can left the size blank for coloumn but for row you have to define it.
     int[][] arr1 = {
-        {1, 2, 3},
-        {2, 5, 7}
-    };
+        {1, 2, 3}, // assume this ia an array of index 0,
+        {2, 5, 7} // this is an array of index 2.
+    }; // It is basically an arrays of arrays.
 }
