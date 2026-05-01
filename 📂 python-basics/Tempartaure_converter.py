8f5d748c5b924589fbcacf2d333aba7435ff1026
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-01 10:00:04 +0530

    Update Tempartaure_converter.py

--- 📂 python-basics/Tempartaure_converter.py
+++ 📂 python-basics/Tempartaure_converter.py
@@ -1,1 +1,15 @@
+# Temperature Converter
 
+unit = input("Is it in Celsius or Fahrenheit(C or F)?: ")
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = (temp * (9/5) + 32)
+    unit = "F"
+    print(f"Your temperature is {round(temp, 2)}°F")
+elif unit == "F":
+    temp = ((temp - 32) * 5/9)
+    unit = "C"
+    print(f"Your temperature is {round(temp, 2)}°C")
+else:
+    print("Unit is not valid parameter")
