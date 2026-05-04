42457f8dabb04dc310acdbadc92b17a3e023226a
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-04 11:54:34 +0530

    Create String_Some_Methods.py

---
+++ 📂 python-basics/String_Some_Methods.py
@@ -0,0 +1,11 @@
+name = input("Enter your full name: ")
+answer = name.find("i") # this will find occurrences that you want ot find
+answer1 = name.capitalize()
+answer2 = name.upper()
+answer3 = name.lower()
+answer4 = name.isdigit()
+answer5 = name.isalpha()
+answer6 = name.count("i")
+answer7 = name.replace("i","a")
+
+print(answer1)
